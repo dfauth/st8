662009d79cb1b7cab39e6524861bafe8e7de5678
Damian Fauth <dfauth@gmail.com>
2018-06-13 07:10:10 +0800

    debug

--- src/main/scala/st8/State.scala
+++ src/main/scala/st8/State.scala
@@ -1,15 +1,15 @@
 package st8
 
 case class State[T,U,V](ctx:U, nested:T) {
-  val map = scala.collection.mutable.Map[V, Transition[T,U,V]]()
+  val map = scala.collection.mutable.Map[V, Builder[T,U,V]]()
   def trigger(e: V):State[T,U,V] = {
-    map.get(e).map(t => t.execute()).getOrElse(this)
+    map.get(e).map(t => t.build().execute()).getOrElse(this)
   }
-  def onEntry(f:Function[T, Unit]):State[T,U,V] = {this}
-  def onExit(f:Function[T, Unit]):State[T,U,V] = {this}
+  def onEntry(f:Function[Transition[T,U,V], Unit]):State[T,U,V] = {this}
+  def onExit(f:Function[Transition[T,U,V], Unit]):State[T,U,V] = {this}
   def onEvent(e:V):Builder[T,U,V] = {
     val t = Transition.create[T,U,V](ctx, this)
-    map(e) = t.build()
+    map(e) = t
     t
   }
 }
--- src/main/scala/st8/Transition.scala
+++ src/main/scala/st8/Transition.scala
@@ -1,32 +1,42 @@
 package st8
 
+import org.apache.logging.log4j.scala.Logging
+
 object Transition {
   def create[T,U,V](ctx:U, current:State[T,U,V]) = Builder[T,U,V](ctx, current)
 }
 
-case class Transition[T, U, V](pipline: () => Option[State[T,U,V]], current:State[T,U,V]) {
+case class Transition[T, U, V](pipeline: () => Option[State[T,U,V]], current:State[T,U,V], on_transition:Transition[T,U,V] => Transition[T,U,V]) {
 
   def execute(): State[T,U,V] = {
-    pipline().getOrElse(current)
+    val result:Option[State[T,U,V]] = pipeline()
+    result.foreach(o => {
+      on_transition(this)
+    })
+    result.getOrElse(current)
   }
 }
 
-case class Builder[T, U, V](ctx:U, current:State[T,U,V]) {
+case class Builder[T, U, V](ctx:U, current:State[T,U,V]) extends Logging {
 
   var next:State[T,U,V] = _
-  var on_transition:T => Unit = _
+  var on_transition:Transition[T,U,V] => Transition[T,U,V] = t => {
+    logger.info("identity()")
+    t
+  }
   var guard:U => State[T,U,V] => Boolean = _
   var pipeline:Option[State[T,U,V]] = Option[State[T,U,V]](current)
 
   def build(): Transition[T,U,V] = {
-    Transition[T,U,V](() =>pipeline.filter(guard(ctx)).map(_ => next).map(o=>{
-      //      on_transition(o.nested)
-      o
-    }), current)
+    Transition[T,U,V](() =>pipeline.filter(guard(ctx)).map(_ => next), current, on_transition)
   }
 
-  def onTransition(f: T => Unit):Builder[T,U,V] = {
-    on_transition = f
+  def onTransition(f: Transition[T,U,V] => Unit):Builder[T,U,V] = {
+    this.on_transition = t=>{
+      logger.info("here: "+t)
+      f(t);
+      t
+    }
     this
   }
 

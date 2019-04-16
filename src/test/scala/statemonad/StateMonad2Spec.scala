d1775a693bb3df75b2859398ee6120e30fabcd3f
Damian Fauth <dfauth@gmail.com>
2019-04-17 07:54:33 +0800

    StateMonadTrafficLightSpec

---
+++ src/test/scala/statemonad/StateMonad2Spec.scala
@@ -0,0 +1,178 @@
+package statemonad
+
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest._
+
+class StateMonad2Spec extends FlatSpec with Matchers with LazyLogging {
+
+  case class StateMonad[S, A](run: S => (A, S)) {
+
+    def map[B](f: A => B): StateMonad[S, B] =
+      StateMonad(s => {
+        val (a, t) = run(s)
+        (f(a), t)
+      })
+
+    def flatMap[B](f: A => StateMonad[S, B]): StateMonad[S, B] =
+      StateMonad(s => {
+        val (a, t) = run(s)
+        f(a) run t
+      })
+
+    def eval(s: S): A =
+      run(s)._1
+  }
+
+  object StateMonad {
+
+    def insert[S, A](a: A): StateMonad[S, A] =
+      StateMonad(s => (a, s))
+
+    def get[S, A](f: S => A): StateMonad[S, A] =
+      StateMonad(s => (f(s), s))
+
+    def mod[S](f: S => S): StateMonad[S, Unit] =
+      StateMonad(s => ((), f(s)))
+  }
+
+  type Transition[A] = StateMonad[A, MyState]
+
+  // states
+  sealed trait MyState
+  case object A extends MyState
+  case object B extends MyState
+  case object C extends MyState
+  case object D extends MyState
+
+  case class State(current:MyState) {
+
+  }
+
+  object Initial extends State(A)
+
+  // side effects
+  sealed trait SideEffect
+  case object SE1 extends SideEffect
+
+  def toStateMonad(pf: PartialFunction[MyState,MyState]):Transition[State] = {
+    StateMonad[State, MyState](s => {
+      val b = if(pf.isDefinedAt(s.current)) {
+        pf(s.current)
+      } else {
+        s.current
+      }
+      (b, State(b))
+    })
+  }
+
+  // events
+  def A1: Transition[State] = toStateMonad {
+    case A => B
+  }
+
+  def A2: Transition[State] = toStateMonad {
+    case A => C
+  }
+
+  def B1: Transition[State] = toStateMonad {
+    case B => C
+  }
+
+  def B2: Transition[State] = toStateMonad {
+    case B => D
+  }
+
+  def C1: Transition[State] = toStateMonad {
+    case C => D
+  }
+
+  "A State" should "be able to handle this" in {
+    /**
+      *        A
+      *      /  \
+      *    A1   A2
+      *   /      \
+      *  B---B1---C
+      *   \      /
+      *    B2  C1
+      *     \ /
+      *      D
+      */
+      {
+        // initial state
+        val sm = StateMonad.insert[State, MyState](A)
+        sm.eval(Initial) should be (A)
+
+        // current state
+        val sc = StateMonad[State, MyState](c => (c.current, c))
+        val current = sc.eval(Initial)
+        current should be (A)
+
+        {
+          val next = for {
+            next <- A1
+          } yield next
+            next.eval(Initial) should be (B)
+        }
+
+        {
+          var next2 = for {
+            next <- A1
+            next1 <- B1
+          } yield next1
+          next2.eval(Initial) should be (C)
+        }
+
+        {
+          var next = for {
+            next <- A1
+            next <- B1
+            next <- C1
+          } yield next
+          next.eval(Initial) should be (D)
+        }
+
+        {
+          var next = for {
+            next <- A1
+            next <- B2
+          } yield next
+          next.eval(Initial) should be (D)
+        }
+
+        {
+          var next = for {
+            next <- A2
+            next <- A1
+          } yield next
+          next.eval(Initial) should be (C)
+        }
+
+        {
+          var next = for {
+            next <- A2
+            next <- C1
+          } yield next
+          next.eval(Initial) should be (D)
+        }
+
+        {
+          var next = for {
+            next <- A2
+            next <- C1
+            next <- A1
+          } yield next
+          next.eval(Initial) should be (D)
+        }
+
+//        {
+//          val events:Seq[StateMonad[State, MyState]] = Seq[StateMonad[State, MyState]](A2,C1,A1)
+//
+//          events.foldLeft(sm)((sm, e) => {
+//            sm.flatMap(t(e))
+//          }).eval(Initial) should be (D)
+//
+//        }
+      }
+  }
+}
